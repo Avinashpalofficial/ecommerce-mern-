8b81680e5088300add4417686280f74c8099893c
Avinashpalofficial <apal73451@gmail.com>
2025-10-23 08:31:36 +0530

    userschema

---
+++ backend/Models/userSchema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userschema = new mongoose.Schema({
+     username:{
+        type:String,
+        required:true,
+        unique:true
+     },
+     email:{
+        type:String,
+        required:true,
+        unique:true
+     },
+     password:{
+        type:String,
+        required:true,
+        minlength:8
+     },
+     firstName:String,
+     lastName:String,
+     dateofBirth:Date,
+     roles:{
+        type:[String],  //Array of string for roles
+        default:['user']
+     },
+  
+},{ timestamps:true})
+
+ 
+const User = mongoose.model('User',userschema)
+
+export default User
