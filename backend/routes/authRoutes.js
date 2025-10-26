6b5886fb7ce6e2514a0d9eece7720047a066a07b
Avinashpalofficial <apal73451@gmail.com>
2025-10-26 13:19:06 +0530

    create a fotgot password route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 // import authUser from '../middleware/auth.js'
-import {registerUser,loginUser ,logoutUser} from '../controllers/authController.js'
+import {registerUser,loginUser ,logoutUser,forgotPassword} from '../controllers/authController.js'
 
 const router = express.Router()
 
 router.post('/register',registerUser)
 router.post('/login',loginUser)
-router.post('/logout',logoutUser)
+router.get('/logout',logoutUser)
+router.post('/password/forgot',forgotPassword)
 
 export default router
