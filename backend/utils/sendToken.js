9a26d0a88f70e4ac9c0810ff261a5fd30514b38d
Avinashpalofficial <apal73451@gmail.com>
2025-10-26 07:01:27 +0530

    create token

---
+++ backend/utils/sendToken.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+    const sendToken = (user, statusCode, res) => {
+    const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_TIME,
+    }
+  );
+
+  console.log("toekn=", token);
+  const cookieExpire = Number(process.env.COOKIE_EXPIRES_TIME) || 7;
+  const options = {
+    expires: new Date(Date.now() + cookieExpire * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  };
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, user, token });
+};
+
+export default sendToken;
