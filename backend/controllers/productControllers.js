d5ee04eb351091f70f55ea29006221cb9048582f
Avinashpalofficial <apal73451@gmail.com>
2025-10-27 21:46:24 +0530

    productfeatures

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,44 +1,64 @@
- import Product from '../Models/product.js'
+import Product from "../Models/product.js";
+import APIfeatures from "../utils/apiFeatures.js";
+import catchAsyncError from "../middleware/asyncError.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import cloudinary from "../config/cloudinary.js";
 
-import catchAsyncError from '../middleware/asyncError.js'
-import ErrorHandler  from '../utils/errorHandler.js'
-import cloudinary from '../config/cloudinary.js'
+const newProduct = catchAsyncError(async (req, res, next) => {
+    // Check if images are provided
+  if (!req.files || req.files.length === 0) {
+    return next(new ErrorHandler("At least one image upload", 400));
+  }
+  let imageLinks = [];
+  //upload images in cloudinary
+  for (let i = 0; i < req.files.length; i++) {
+    //convert buffer to base64
+    const b64 = Buffer.from(req.files[i].buffer).toString("base64");
+    const dataURL = "data;" + req.files[i].mimetype + ";base64" + b64;
+    //upload to  cloudinary
+    const result = await cloudinary.uploader.upload(dataURL, {
+      folder: ecommerce / products,
+    });
+    imageLinks.push({
+      public_id: result.public_id,
+      url: result.url,
+    });
+  }
+  const productData = {
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    category: req.body.category,
+    stocks: req.body.stocks || 0,
+    images: imageLinks,
+    user: req.user.id,
+  };
+  const product = await Product.create(productData);
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+//Get all Products
+const getProducts = catchAsyncError(async (req, res, next) => {
+  const resPerPage = 8;
+  const TotalProducts = await Product.countDocuments();
+  const apiFeatures = new APIfeatures(Product.find(), req.query);
+  search();
+  Filter();
+  //Count filtered products without pagination
+  const filterProductCount = await Product.countDocuments(
+    apiFeatures.query.getQuery()
+  );
+  apiFeatures.pagination(resPerPage);
+  const products = await apiFeatures.query;
+  res.status(200).json({
+    success: true,
+    TotalProducts,
+    resPerPage,
+    filterProductCount,
+    products,
+  });
+});
 
-
-const newProduct = catchAsyncError(async(req,res,next)=>{
-    //  // Check if images are provided
-    if(!req.files  || req.files.length===0){
-        return next(new ErrorHandler('At least one image upload',400) )
-    }
-      let imageLinks= []
-       //upload images in cloudinary
-      for(let i =0; i<req.files.length;i++){
-        //convert buffer to base64
-        const b64 =  Buffer.from(req.files[i].buffer).toString('base64')
-        const dataURL = 'data;'+ req.files[i].mimetype + ";base64"+b64
-        //upload to  cloudinary
-         const result=     await cloudinary.uploader.upload(dataURL,{
-                folder:ecommerce/products
-              })
-        imageLinks.push({
-            public_id : result.public_id,
-            url:result.url
-        })      
-      }
-      const productData =  {
-           name: req.body.name,
-           price:req.body.price,
-           description:req.body.description,
-           category:req.body.category,
-           stocks:req.body.stocks || 0 ,
-           images:imageLinks,
-           user:req.user.id
-      }
-      const product = await Product.create(productData)
-      res.status(200).json({
-        success:true,
-        product
-      })
-})
-
-export default {newProduct}
+export default { newProduct, getProducts };
