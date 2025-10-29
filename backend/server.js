387e5c899ccd93fad09e09cff4ad4d01a08f18b1
Avinashpalofficial <apal73451@gmail.com>
2025-10-29 15:03:04 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import router from './routes/authRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 import cookieParser from "cookie-parser";
-import errorMiddleware from './middleware/error.js';
+import errorMiddleware from "./middleware/error.js";
 
 //load environment modules
-dotenv.config()
+dotenv.config();
+
 //connect to mongoDB
-connectDB()
+connectDB();
 //initialize express App
-const app = express()
-app.use(cookieParser())
-app.use(express.json())
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/v1/auth',router )
-app.get('/',(req,res)=>{
-    res.send("project is ready")
-})
-app.use(errorMiddleware)
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    
-})
-
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1", productRouter);
+app.get("/", (req, res) => {
+  res.send("project is ready");
+});
+app.use(errorMiddleware);
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
