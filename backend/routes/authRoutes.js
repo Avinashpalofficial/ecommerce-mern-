e8158d5bf20919a4ed64c7ef7147ab10746d756f
Avinashpalofficial <apal73451@gmail.com>
2025-10-29 15:01:07 +0530

    update authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,21 @@
-import express from 'express'
+import express from "express";
 // import authUser from '../middleware/auth.js'
-import {registerUser,loginUser ,logoutUser,forgotPassword,resetpassword,loginAdmin} from '../controllers/authController.js'
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  forgotPassword,
+  resetpassword,
+} from "../controllers/authController.js";
+import adminLogin from "../controllers/adminController.js";
 
-const router = express.Router()
+const authRouter = express.Router();
 
-router.post('/register',registerUser)
-router.post('/login',loginUser)
-router.get('/logout',logoutUser)
-router.post('/password/forgot',forgotPassword)
-router.put('/password/reset/:token',resetpassword)
-router.post('/admin/login',loginAdmin)
+authRouter.post("/register", registerUser);
+authRouter.post("/login", loginUser);
+authRouter.get("/logout", logoutUser);
+authRouter.post("/password/forgot", forgotPassword);
+authRouter.put("/password/reset/:token", resetpassword);
+authRouter.post("/admin/login", adminLogin);
 
-export default router
+export default authRouter;
