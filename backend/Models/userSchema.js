12bb6bb4a6d0fa6920662b759e69337bd2a1a545
Avinashpalofficial <apal73451@gmail.com>
2025-10-30 15:53:08 +0530

    add order section

--- backend/Models/userSchema.js
+++ backend/Models/userSchema.js
@@ -1,39 +1,40 @@
 import mongoose from "mongoose";
 
-const userschema = new mongoose.Schema({
-     username:{
-        type:String,
-        required:true,
-        unique:true
-     },
-     email:{
-        type:String,
-        required:true,
-        unique:true
-     },
-     password:{
-        type:String,
-        required:true,
-        minlength:8
-     },
-     firstName:String,
-     lastName:String,
-     dateofBirth:Date,
-     roles:{
-        type:[String],  //Array of string for roles
-        default:['user']
-     },
-     orders: [             // 👈 new field
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "Order"
-    }
-  ],
-     resetPasswordToken:String,
-     resetPasswordExpire:Date
-  
-},{ timestamps:true})
+const userschema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    firstName: String,
+    lastName: String,
+    dateofBirth: Date,
+    roles: {
+      type: [String], //Array of string for roles
+      default: ["user"],
+    },
+    orders: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Order",
+      },
+    ],
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
 
-
-const User = mongoose.model('User',userschema)
-export default User
+const User = mongoose.model("User", userschema);
+export default User;
