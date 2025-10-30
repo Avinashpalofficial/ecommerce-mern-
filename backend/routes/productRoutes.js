9751c180a15f4d5deb092b596916f94fae7f8ddb
Avinashpalofficial <apal73451@gmail.com>
2025-10-30 15:55:20 +0530

    update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,27 @@
 import express from "express";
-import { createProduct,getSingleProduct,updateProduct,deleteProduct } from "../controllers/productControllers.js";
+import {
+  createProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productControllers.js";
 import { authUser, authAdmin } from "../middleware/auth.js";
-import {upload} from '../config/cloudinary.js'
+import { upload } from "../config/cloudinary.js";
 const productRouter = express.Router();
 
-productRouter.post("/admin/product/new", authAdmin,upload.array('images',5), createProduct);
-productRouter.get('/product/:id',getSingleProduct)
-productRouter.put('/admin/products/:id',authAdmin,upload.array('images',5),updateProduct)
-productRouter.delete('/admin/products/:id',authAdmin,deleteProduct)
+productRouter.post(
+  "/admin/product/new",
+  authAdmin,
+  upload.array("images", 5),
+  createProduct
+);
+productRouter.get("/product/:id", getSingleProduct);
+productRouter.put(
+  "/admin/products/:id",
+  authAdmin,
+  upload.array("images", 5),
+  updateProduct
+);
+productRouter.delete("/admin/products/:id", authAdmin, deleteProduct);
 
 export default productRouter;
