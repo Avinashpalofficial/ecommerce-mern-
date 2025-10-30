394e3498a2734fac235bb04f1b1dec6d9cde9ecc
Avinashpalofficial <apal73451@gmail.com>
2025-10-30 14:21:25 +0530

    deleteProduct

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -4,6 +4,7 @@ import catchAsyncError from "../middleware/asyncError.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import cloudinary from "../config/cloudinary.js";
 import { upload } from "../config/cloudinary.js";
+import { success } from "zod";
 
 export const createProduct = catchAsyncError(async (req, res, next) => {
   // Check if images are provided
@@ -95,3 +96,15 @@ export const updateProduct = catchAsyncError(async (req, res, next) => {
     product,
   });
 });
+//delete Product /api/v1/admin/product/:id
+ export const deleteProduct = catchAsyncError(async(req,res,next)=>{
+                   const product = await Product.findById(req.params.id)
+                   if(!product){
+                       return next(new ErrorHandler('Product not found',401))
+                   }
+           for(let i=0;i<product.images.length;i++){
+             await cloudinary.uploader.destroy(product.images[i].public_id)
+           }    
+           await product.deleteOne()
+              res.status(200).json({success:true,message:"Prodcut delete successfully"}) 
+ }) 
