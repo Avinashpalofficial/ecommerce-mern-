7a70b8113502da3e225ed921d8f8b5e1342b2e46
Avinashpalofficial <apal73451@gmail.com>
2025-10-30 15:58:18 +0530

    update code

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,20 @@ import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middleware/error.js";
+import OrderRouter from "./routes/orderRoutes.js";
+import { upload } from "./config/cloudinary.js";
+import cloudinary from "./config/cloudinary.js";
 
 //load environment modules
 dotenv.config();
 
 //connect to mongoDB
 connectDB();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 //initialize express App
 const app = express();
 app.use(cookieParser());
@@ -19,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1", productRouter);
+app.use("/api/v1", OrderRouter);
 app.get("/", (req, res) => {
   res.send("project is ready");
 });
