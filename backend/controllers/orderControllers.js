59172ec6c3e0a9af64c858168e2ec420f4f65ef7
Avinashpalofficial <apal73451@gmail.com>
2025-11-01 06:54:01 +0530

    getSingleOrder

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,7 +1,9 @@
 
+import ErrorHandler from "../utils/errorHandler.js";
 import catchAsyncError from "../middleware/asyncError.js";
 import Order from "../Models/Order.js";
 import User from "../Models/userSchema.js";
+import { success } from "zod";
 
 export const newOrder= catchAsyncError(async(req ,res ,next)=>{
                const{  orderItems,
@@ -26,4 +28,12 @@ export const newOrder= catchAsyncError(async(req ,res ,next)=>{
             $push:{orders:order._id}
         })
         res.status(200).json({success:true,message:order})
+})
+//Get single order//  api/v1/orders/:id
+export const getSingleOrder=  catchAsyncError(async(req,res,next)=>{
+                     const order = await Order.findById(req.params.id).populate('user', 'name email')
+                     if(!order){
+                            return next(new ErrorHandler('Order not found',401))
+                     }
+                     res.status(200).json({success:true,order})
 })
