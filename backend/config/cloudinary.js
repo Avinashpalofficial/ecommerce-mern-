16050ed1ab483a22b5db6b61f6133f9f759e64f6
Avinashpalofficial <apal73451@gmail.com>
2025-11-02 08:54:36 +0530

    update code

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,6 +2,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import pkg from 'multer-storage-cloudinary';
 const {CloudinaryStorage}  =pkg
 import multer from 'multer'
+import ErrorHandler from '../utils/errorHandler.js';
 
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -11,10 +12,21 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary:cloudinary,
     params:{
-        folder:'ecommerce',
+        folder:'ecommerce/products',
         allowed_formats:['jpg','jpeg','png','webp'],
         transformation: [{ width: 500, height: 500, crop: 'limit' }]
     }
 })
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage: storage,
+    limits:{
+         fileSize: 5 * 1024 * 1024 // 5MB
+    },
+     fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(new ErrorHandler('Only image files are allowed!', 400), false);
+        }
+    }
+ });
 export default cloudinary;
