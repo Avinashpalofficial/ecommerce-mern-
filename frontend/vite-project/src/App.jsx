bf453d2cf417e9de7bb8f1b67d0af7ba964cad76
Avinashpalofficial <apal73451@gmail.com>
2026-01-21 13:05:13 +0530

    update routes

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -12,11 +12,17 @@ import MyProfile from './pages/Myprofile'
 import ProductCard from './ProductCard/Productdetail.jsx'
 import PaymentSection from './Payment/PaymentSection'
 import Cart from './cart/Cart'
-import ProductList from './ProductCard/Productlist'
-  
-   
+import ProductList from './ProductCard/Productlist'  
 import ProductDetail from './ProductCard/Productdetail.jsx'
 import OrderSuccess from './pages/OrderSuccess.jsx'
+import Myorders from './pages/Myorders.jsx'
+import SendOtp from './pages/SendOtp.jsx'
+import VerifyOtp from './pages/VerifyOtp.jsx'
+import LoginSecurity from './pages/LoginSecurity.jsx'
+import AddressForm from './pages/AddressForm.jsx'
+import CloverSignup from './pages/BusinessAccount.jsx'
+import CloverWallet from './pages/Wallet.jsx'
+import CloverSupportChoice from './pages/CloverSupport.jsx'
 
 const router = createBrowserRouter([
   {
@@ -66,9 +72,41 @@ const router = createBrowserRouter([
     
     {
         path:'/cart',
-        element:<><Cart/></>
-    }
-
+        element:<><ComplexNavbar/><Cart/><Footer/></>
+    },
+    {
+        path:'/my-orders',
+        element:<><ComplexNavbar/><Myorders/><Footer/></>
+    },
+    {
+        path:'/send-otp',
+        element:<><ComplexNavbar/><SendOtp/><Footer/></>
+    },
+    {
+        path:'/verify-otp',
+        element:<><ComplexNavbar/><VerifyOtp/><Footer/></>
+    },
+    {
+        path:'/edit-profile',
+        element:<><ComplexNavbar/><LoginSecurity/><Footer/></>
+    },
+    {
+        path:'/address',
+        element:<><ComplexNavbar/><AddressForm/><Footer/></>
+    },
+    {
+        path:'/business-account',
+        element:<><CloverSignup/></>
+    },
+    {
+        path:'/clover-wallet',
+        element:<><CloverWallet/></>
+    },
+    {
+        path:'/clover-support',
+        element:<><ComplexNavbar/><CloverSupportChoice/><Footer/></>
+    },
+          
 ])
 const App = () => {
   return (
