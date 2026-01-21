5c2a7e2ae62bcda50e3bbde1132df1f842f70d77
Avinashpalofficial <apal73451@gmail.com>
2026-01-21 12:59:29 +0530

    update routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { newOrder,getSingleOrder } from '../controllers/orderControllers.js'
+import { newOrder,getSingleOrder, myOrders } from '../controllers/orderControllers.js'
 import { authUser, authAdmin } from "../middleware/auth.js";
 
  const OrderRouter= express.Router() 
 
 OrderRouter.post('/orders/new',authUser,newOrder)
+OrderRouter.get('/orders/me',authUser,myOrders)
+
 OrderRouter.get('/orders/:id',authUser,getSingleOrder)
+OrderRouter.use((req,res,next)=>{
+  console.log("ORDER ROUTE HIT:", req.originalUrl);
+  next();
+});
 
 export default OrderRouter
