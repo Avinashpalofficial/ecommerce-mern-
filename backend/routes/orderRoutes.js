7480241e34f52f91d965e2f0fcce1a0cfc4e1dc8
Avinashpalofficial <apal73451@gmail.com>
2026-01-25 23:15:28 +0530

    upate order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { newOrder,getSingleOrder, myOrders } from '../controllers/orderControllers.js'
+import { newOrder,getSingleOrder, myOrders, allOrders, UpdateOrder, deleteOrder } from '../controllers/orderControllers.js'
 import { authUser, authAdmin } from "../middleware/auth.js";
 
  const OrderRouter= express.Router() 
@@ -8,6 +8,11 @@ OrderRouter.post('/orders/new',authUser,newOrder)
 OrderRouter.get('/orders/me',authUser,myOrders)
 
 OrderRouter.get('/orders/:id',authUser,getSingleOrder)
+OrderRouter.get('/admin/orders/:id',authAdmin,getSingleOrder)
+OrderRouter.get('/admin/orders',authAdmin,allOrders)
+OrderRouter.put('/admin/orders/:id',authAdmin,UpdateOrder)
+OrderRouter.delete('/admin/orders/:id',authAdmin,deleteOrder)
+
 OrderRouter.use((req,res,next)=>{
   console.log("ORDER ROUTE HIT:", req.originalUrl);
   next();
