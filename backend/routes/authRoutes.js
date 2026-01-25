cb49128ad4893a93f453edf9169e166d6f8ac9d2
Avinashpalofficial <apal73451@gmail.com>
2026-01-25 23:15:00 +0530

    update route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,8 +12,9 @@ import {
   verifyOldEmailOtp,
   verifyNewEmailOtp,
   updateName,
+  
 } from "../controllers/authController.js";
-import adminLogin from "../controllers/adminController.js";
+import  {adminLogin,adminLogout} from '../controllers/adminController.js'
 import { authUser } from "../middleware/auth.js";
 
 const authRouter = express.Router();
@@ -24,12 +25,11 @@ authRouter.get("/user/logout", logoutUser);
 authRouter.post("/password/forgot", forgotPassword);
 authRouter.put("/password/reset/:token", resetpassword);
 authRouter.post("/admin/login", adminLogin);
+authRouter.post("/admin/logout", adminLogout);
 authRouter.post('/email/send-otp',sendEmailOtp)
 authRouter.post('/email/verify-otp',verifyEmailOtp)
 authRouter.post('/email/change',authUser,requestEmailChange)
 authRouter.post('/email/verify-old',authUser,verifyOldEmailOtp)
 authRouter.post('/email/verify-new',authUser,verifyNewEmailOtp)
 authRouter.put("/user/update-name", authUser, updateName);
-
-
 export default authRouter;
