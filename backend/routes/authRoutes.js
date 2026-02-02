4273b25c4d30cc5e20adde1cbe52768269075d5c
Avinashpalofficial <apal73451@gmail.com>
2026-02-02 23:29:54 +0530

    create api for getuserprofile

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,20 +12,23 @@ import {
   verifyOldEmailOtp,
   verifyNewEmailOtp,
   updateName,
+  getUserProfile,
   
 } from "../controllers/authController.js";
-import  {adminLogin,adminLogout} from '../controllers/adminController.js'
-import { authUser } from "../middleware/auth.js";
+import  {adminLogin,adminLogout, getAdminProfile} from '../controllers/adminController.js'
+import { authAdmin, authUser } from "../middleware/auth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/user/register", registerUser);
 authRouter.post("/user/login", loginUser);
 authRouter.get("/user/logout", logoutUser);
+authRouter.get("/user/me",authUser,getUserProfile);
 authRouter.post("/password/forgot", forgotPassword);
 authRouter.put("/password/reset/:token", resetpassword);
 authRouter.post("/admin/login", adminLogin);
-authRouter.post("/admin/logout", adminLogout);
+authRouter.get("/admin/logout", adminLogout);
+authRouter.get("/admin/me",authAdmin,getAdminProfile);
 authRouter.post('/email/send-otp',sendEmailOtp)
 authRouter.post('/email/verify-otp',verifyEmailOtp)
 authRouter.post('/email/change',authUser,requestEmailChange)
