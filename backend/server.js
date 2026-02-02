4e5cae9057d892f1c4a15624c057a73fa8fc62af
Avinashpalofficial <apal73451@gmail.com>
2026-02-02 23:31:35 +0530

    update the routes

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,16 @@ import paymentRouter from "./routes/paymentRoutes.js";
 import { upload } from "./config/cloudinary.js";
 import cloudinary from "./config/cloudinary.js";
 import { stripeWebhook } from "./controllers/paymentControllers.js";
-
+import { DashboardRouter } from "./routes/dashboardRoute.js";
 import cors from "cors";
+
 //load environment modules
 dotenv.config();
 
 //connect to mongoDB
 connectDB();
 const app = express();
+app.use(cookieParser());
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -27,28 +29,32 @@ cloudinary.config({
 //initialize express App
 app.use(
   cors({
-       origin:"http://localhost:5173",
+       origin:[
+         
+         "http://localhost:5173",
+      ],
        credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 )
-app.use(cookieParser());
-app.post(
-  "/api/v1/payment/webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhook
-);
-
 app.use(express.json());
-
 const PORT = process.env.PORT;
+
+
+
+
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1", productRouter);
 app.use("/api/v1", OrderRouter);
 app.use("/api/v1",paymentRouter)
-
+app.use("/api/v1",DashboardRouter)
+app.post(
+  "/api/v1/payment/webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebhook
+);
 app.get('/api/message',(req,res)=>{
         res.json({message:'Backend se response aaya'})
 })
