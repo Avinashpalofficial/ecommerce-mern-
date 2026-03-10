0772e6e8a77e3d2fd405138b55160216492d1282
Avinashpalofficial <apal73451@gmail.com>
2026-03-10 09:52:07 +0530

    udpate the main

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -7,16 +7,19 @@ import { ThemeProvider } from '@material-tailwind/react'
 import {createBrowserRouter} from 'react-router-dom'
 import { CartProvider } from './context/CartContext.jsx'
 import { ProductProvider } from './context/ProductContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <createBrowserRouter>
-    <ThemeProvider>
+    <AuthProvider>
+           <ThemeProvider>
         <ProductProvider>
           <CartProvider>
          <App/>
          </CartProvider>
         </ProductProvider>
     </ThemeProvider>
+    </AuthProvider>
  </createBrowserRouter>
    
 )
