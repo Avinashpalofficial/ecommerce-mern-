0928d8dd229d2f61a9219d54a97ae5bedb7762a9
Avinashpalofficial <apal73451@gmail.com>
2026-03-10 12:44:23 +0530

    update the code

--- frontend/vite-project/src/pages/SendOtp.jsx
+++ frontend/vite-project/src/pages/SendOtp.jsx
@@ -22,7 +22,7 @@ export default function SendOtp() {
       setLoading(true);
 
       const res = await axios.post(
-        "http://localhost:3000/api/v1/auth/email/send-otp",
+        `${import.meta.env.VITE_API_URL}/api/v1/auth/email/send-otp`,
         { email }
       );
 
