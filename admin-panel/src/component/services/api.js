eb85c6e9ce18f9398f984a175e824d88be0f0178
Avinashpalofficial <apal73451@gmail.com>
2026-03-10 09:36:23 +0530

    update the api code

--- admin-panel/src/component/services/api.js
+++ admin-panel/src/component/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios'
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL 
 
  export const api = axios.create({
         baseURL:API_URL,
@@ -10,14 +10,4 @@ const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api"
             "Content-Type" : "application/json",
         }
 })
-// Response interceptor (only for auth error)
-api.interceptors.response.use(
-    (response)=> response,
 
-    (error)=>{
-        if(error.response?.status===401){
-           window.location.href = "/login"
-        }
-        return Promise.reject(error)
-    }
-)
--- admin-panel/src/component/services/auth.js
+++ admin-panel/src/component/services/auth.js
@@ -1,38 +1,54 @@
-import axios from "axios"
-import { api } from "./api"
-import  {toast} from "react-toastify"
+// import axios from "axios"
+// import { api } from "./api"
+// import  {toast} from "react-toastify"
 
-export const login =  async(email,password)=>{
-                    try {
-                        const res = await api.post('/auth/admin/login',{
-                            email,
-                            password
-                        },
+
+// export const adminlogin =  async(email,password)=>{
+//                     try {
+//                         const res = await api.post('/auth/admin/login',{
+//                             email,
+//                             password
+//                         },
+//                     console.log("RESPONSE:",res.data)
                     
-                )
-                if(res.data.success){
+//                 )
+//                 if(res.data.success){
                     
-                   toast.success("login successfully")
-                }else{
-                  toast.error("login failed")   
-                }
-                   return res.data
-                    } catch (error) {
-                         toast.error(error.response?.data?.message || "login failed")
-                         throw error
-                    }
-}
-export  const logout =   async()=>{
-      try {
-                 const res =  await api.post('/auth/admin/logout')
-                 if(res.data.success){
-                    toast.success('logout successfully!')
-                 }
-                 else{
-                    toast.error("failed")
-                 }
-      } catch (error) {
-                         toast.error(error.response?.data?.message || "logout failed")
+//                    toast.success("login successfully")
+//                 }else{
+//                   toast.error("login failed")   
+//                 }
+//                    return res.data
+//                     } catch (error) {
+//                          toast.error(error.response?.data?.message || "login failed")
+//                          throw error
+//                     }
+// }
+// export  const adminlogout =   async()=>{
+//       try {
+//                  const res =  await api.post('/auth/admin/logout')
+//                  if(res.data.success){
+//                     toast.success('logout successfully!')
+//                  }
+//                  else{
+//                     toast.error("failed")
+//                  }
+//       } catch (error) {
+//                          toast.error(error.response?.data?.message || "logout failed")
         
-      }
-}
+//       }
+  
+// }
+
+//    export const getAdminProfile = async()=>{
+//                           try {
+//                             const res =  await api.get("/auth/admin/me")
+//                             console.log("response:",res.data.admin);
+                            
+//                            return res.data.admin
+//                           } catch (error) {
+//                                toast.error(error.response?.data?.message || "invalid admin")
+//                                  return null;
+//                           }
+
+//    }
